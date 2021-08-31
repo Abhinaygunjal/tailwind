ac19b43fa3d9f430b9cb473a5c199b4a9a14628d
Abhinay Gunjal <abhinaygunjal77@gmail.com>
2021-08-31 12:31:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      orange:"colors.orange",
+    },
+    fontFamily:{
+spa:['Spartan']
+    },
+  },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
